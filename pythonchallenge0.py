a49c30575430b0e1eabbba047e30b72986719b7c
Arun@Home <arunisnowhere@gmail.com>
2017-02-24 20:52:20 +0200

    Rename files

---
+++ pythonchallenge0.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Feb 24 11:24:45 2017
+@author: arun
+The challenges from 
+http://www.pythonchallenge.com/
+
+"""
+# Python Challenge 1
+# Completed: February 24, 2017
+
+print (2**38)
