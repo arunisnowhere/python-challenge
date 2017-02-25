b0d0655ae92f609c41aa5e14dcb05858d3392603
Arun@Home <arunisnowhere@gmail.com>
2017-02-26 00:20:09 +0200

    Clean codes

--- pythonchallenge0.py
+++ pythonchallenge0.py
@@ -4,9 +4,8 @@ Created on Fri Feb 24 11:24:45 2017
 @author: arun
 The challenges from 
 http://www.pythonchallenge.com/
-
 """
-# Python Challenge 1
+# Python Challenge 0
 # Completed: February 24, 2017
 
 print (2**38)
--- pythonchallenge1.py
+++ pythonchallenge1.py
@@ -6,47 +6,54 @@ The challenges from
 http://www.pythonchallenge.com/
 """
 
-# Python Challenge 2
+# Python Challenge 1
 # Completed: February 24, 2017
 # Problem transferred independently to another project and expanded / continued
 
-inputcode =  "g fmnc wms bgblr rpylqjyrc gr zw fylb. rfyrq ufyr amknsrcpq ypc dmp. bmgle gr gl zw fylb gq glcddgagclr ylb rfyr'q ufw rfgq rcvr gq qm jmle. sqgle qrpgle.kyicrpylq() gq pcamkkclbcb. lmu ynnjw ml rfc spj"
-outputanswer = ''
-# The number of letters by which the alphabet is shifted.
-# A = C means keyshift = 2
-keyshift = 2
-
 # Two methods
 # Method 1
-for character in inputcode:
-    # if char > a or if char < z
-    if (ord(character) >= ord('a') and ord(character) <= ord('z')):
-        outputanswer = outputanswer + chr(ord('a') + (ord(character) + keyshift - ord('a'))%26)
-    # in this case, simply add the symbol
-    else:
-        outputanswer = outputanswer + character
-print (outputanswer)
+def caesar_cipher1(inputcode, keyshift):
+    outputanswer = ''
+    for character in inputcode:
+        # if char > a or if char < z
+        if (ord(character) >= ord('a') and ord(character) <= ord('z')):
+            outputanswer = outputanswer + chr(ord('a') + (ord(character) + keyshift - ord('a'))%26)
+        # in this case, simply add the symbol
+        else:
+            outputanswer = outputanswer + character
+    return outputanswer   
+
 
 # Method 2
 # Python alternative using str.maketrans(intab, outtab)
-# Completed: May 04, 2016
-import string
-
 # Two methods
-# Method 1
-alphabet = 'abcdefghijklmnopqrstuvwxyz'
-code = ''
-for i in range(0,26):
-    code = code + chr(97+(i + keyshift)%26)
-    
-trantab = str.maketrans(alphabet, code)
-print (inputcode.translate(trantab))
+# Method 2.1
+def caesar_cipher2_1(inputcode, keyshift):    
+    alphabet = 'abcdefghijklmnopqrstuvwxyz'
+    code = ''
+    for i in range(0,26):
+        code = code + chr(97+(i + keyshift)%26)    
+    trantab = str.maketrans(alphabet, code)
+    return (inputcode.translate(trantab))
 
-# Method 2
-table = str.maketrans(string.ascii_lowercase, string.ascii_lowercase[keyshift:]+string.ascii_lowercase[:keyshift])
-print (str.translate(inputcode, table))
-    
-# Python Challenge 3
+# Method 2.2
+def caesar_cipher2_2(inputcode, keyshift):
+    import string
+    table = str.maketrans(string.ascii_lowercase, string.ascii_lowercase[keyshift:]+string.ascii_lowercase[:keyshift])
+    return (str.translate(inputcode, table))
 
-    
+
+inputcode =  "g fmnc wms bgblr rpylqjyrc gr zw fylb. rfyrq ufyr amknsrcpq ypc dmp. bmgle gr gl zw fylb gq glcddgagclr ylb rfyr'q ufw rfgq rcvr gq qm jmle. sqgle qrpgle.kyicrpylq() gq pcamkkclbcb. lmu ynnjw ml rfc spj"
+# The number of letters by which the alphabet is shifted.
+keyshift = 2 # A = C means keyshift = 2
+
+print (caesar_cipher1(inputcode, keyshift))
+print (caesar_cipher2_1(inputcode, keyshift))
+print (caesar_cipher2_2(inputcode, keyshift))
+
+inputcode = "map"
+keyshift = 2
+print (caesar_cipher1(inputcode, keyshift))
+print (caesar_cipher2_1(inputcode, keyshift))
+print (caesar_cipher2_2(inputcode, keyshift)) 
 
